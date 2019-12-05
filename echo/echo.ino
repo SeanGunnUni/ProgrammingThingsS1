21c37f050a5a9a6bc93f3cc47614efaf2af30743
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2019-12-05 15:11:54 +0000

    Update echo.ino

--- echo/echo.ino
+++ echo/echo.ino
@@ -10,6 +10,11 @@ Zumo32U4Motors motors;
 Zumo32U4LineSensors lineSensors;
 Zumo32U4ProximitySensors proxSensors;
 
+//
+#define NUM_SENSORS 5
+unsigned int lineSensorValues[NUM_SENSORS];
+//
+
 uint16_t lineSensorValues[5] = { 0, 0, 0, 0, 0 };
 
 bool proxLeftActive;
@@ -27,8 +32,8 @@ void setup() {
   //motors.flipRightMotor(true);
 
   //sensors
-  lineSensors.initThreeSensors();
-  proxSensors.initThreeSensors();
+  lineSensors.initFiveSensors();
+  proxSensors.initFrontSensor();
    /* After setting up the proximity sensors with one of the
    * methods above, you can also customize their operation: */
   //proxSensors.setPeriod(420);
@@ -36,8 +41,7 @@ void setup() {
   //proxSensors.setPulseOffTimeUs(578);
   //uint16_t levels[] = { 4, 15, 32, 55, 85, 120 };
   //proxSensors.setBrightnessLevels(levels, sizeof(levels)/2);
-  
-  calibrateLineSensors();
+  lineSensors.calibrate();
 
   delay(1000);
 }
@@ -48,53 +52,11 @@ void loop() {
   int incomingByte = 0; // for incoming serial data
   
   // send data only when you receive data:
- /* if (Serial1.available() > 0) {
+ if (Serial1.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial1.read();
 
     switch(incomingByte){
-      case 'w':
-      motors.setLeftSpeed(200);
-      motors.setRightSpeed(200);
-      delay(2);
-      break;
-      case 's':
-      motors.setLeftSpeed(0);
-      motors.setRightSpeed(0);
-      delay(2);
-      break;
-      case 'r':
-      motors.setLeftSpeed(200);
-      motors.setRightSpeed(-200);
-      delay(2);
-      break;
-      case 'l':
-      motors.setRightSpeed(200);
-      motors.setLeftSpeed(-200);
-      delay(2);
-      break;
-      case 'b':
-      motors.setRightSpeed(-200);
-      motors.setLeftSpeed(-200);
-      delay(2);
-      break;
-    }
-    // say what you got:
-    //Serial1.print("I received: ");
-    //Serial1.println(incomingByte, DEC);
-    //Serial.print("I received: ");
-    //Serial.println(incomingByte, DEC);*/
-      static uint16_t lastSampleTime = 0;
-
-  if ((uint16_t)(millis() - lastSampleTime) >= 100)
-  {
-    lastSampleTime = millis();
-
-     if (Serial1.available() > 0) {
-      // read the incoming byte:
-      incomingByte = Serial1.read();
-  
-      switch(incomingByte){
         case 'w':
         forwardMoving();
         break;
@@ -110,8 +72,12 @@ void loop() {
         case 'b':
         backwardMoving();
         break;
-      }
-  
+    }
+  }
+  static uint16_t lastSampleTime = 0;
+  if ((uint16_t)(millis() - lastSampleTime) >= 100)
+  {
+    lastSampleTime = millis();
       // Send IR pulses and read the proximity sensors.
       proxSensors.read();
   
@@ -125,21 +91,9 @@ void loop() {
   
       // Send the results to the LCD and to the serial monitor.
       printReadingsToSerial();
-    }
-  }
+  } 
 }
-void calibrateLineSensors()
-{
 
-   Serial1.println("Calibrating");
-   Serial.println("Calibrating");
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    lineSensors.calibrate();
-  }
-   Serial1.println("Calibrated");
-   Serial.println("Calibrated");
-}
 void printReadingsToSerial()
 {
   static char buffer[80];
@@ -172,7 +126,8 @@ void mazeSolve()
 
   while(1)
   {
-    if(aboveDarkSpot())
+    uint8_t a = readSensors();
+    if(!aboveLineDark(a))
     {
       // We found the end of the maze, so we succeeded in solving
       // the maze.
@@ -216,22 +171,98 @@ void forwardMoving(){
   motors.setLeftSpeed(200);
   motors.setRightSpeed(200);
   delay(2);
+  uint8_t a = readSensors();
+  if(!aboveLineDark(a)){
+    stopMoving();
+  }
 }
 
 void leftMoving(){
   motors.setRightSpeed(200);
   motors.setLeftSpeed(-200);
   delay(2);
+    uint8_t a = readSensors();
+  if(!aboveLineDark(a)){
+    stopMoving();
+  }
 }
 
 void rightMoving(){
   motors.setLeftSpeed(200);
   motors.setRightSpeed(-200);
   delay(2);
+    uint8_t a = readSensors();
+  if(!aboveLineDark(a)){
+    stopMoving();
+  }
 }
 
 void backwardMoving(){
   motors.setRightSpeed(-200);
   motors.setLeftSpeed(-200);
   delay(2);
+    uint8_t a = readSensors();
+  if(!aboveLineDark(a)){
+    stopMoving();
+  }
+}
+
+void backTrack(){
+  }
+
+void turnLeft90(){
+  turnSensorReset();
+  // Turn to the left 90 degrees.
+  motors.setSpeeds(-calibrationSpeed, calibrationSpeed);
+  while((int32_t)turnAngle < turnAngle45 * 2)
+  {
+    lineSensors.calibrate();
+    turnSensorUpdate();
+  }
+ }
+
+void turnRight90(){
+    turnSensorReset();
+  // Turn to the right 90 degrees.
+  motors.setSpeeds(calibrationSpeed, -calibrationSpeed);
+  while((int32_t)turnAngle > -turnAngle45 * 2)
+  {
+    lineSensors.calibrate();
+    turnSensorUpdate();
+  }
+}
+
+void turnSensorReset()
+{
+  gyroLastUpdate = micros();
+  turnAngle = 0;
+}
+
+// Read the gyro and update the angle.  This should be called as
+// frequently as possible while using the gyro to do turns.
+void turnSensorUpdate()
+{
+  // Read the measurements from the gyro.
+  gyro.read();
+  turnRate = gyro.g.z - gyroOffset;
+
+  // Figure out how much time has passed since the last update (dt)
+  uint16_t m = micros();
+  uint16_t dt = m - gyroLastUpdate;
+  gyroLastUpdate = m;
+
+  // Multiply dt by turnRate in order to get an estimation of how
+  // much the robot has turned since the last update.
+  // (angular change = angular velocity * time)
+  int32_t d = (int32_t)turnRate * dt;
+
+  // The units of d are gyro digits times microseconds.  We need
+  // to convert those to the units of turnAngle, where 2^29 units
+  // represents 45 degrees.  The conversion from gyro digits to
+  // degrees per second (dps) is determined by the sensitivity of
+  // the gyro: 0.07 degrees per second per digit.
+  //
+  // (0.07 dps/digit) * (1/1000000 s/us) * (2^29/45 unit/degree)
+  // = 14680064/17578125 unit/(digit*us)
+  turnAngle += (int64_t)d * 14680064 / 17578125;
 }
